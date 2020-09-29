32f87ee25ae47c5a711bd73561d48ed16c48e305
suqing <944068283@qq.com>
2020-09-29 16:21:24 +0800

    添加

--- suqing-practice-master/src/components/TableWorld.vue
+++ suqing-practice-master/src/components/TableWorld.vue
@@ -1,82 +1,89 @@
 <template>
- <div class="table">
-    <el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column
-      label="日期"
-      width="180">
-      <template slot-scope="scope">
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ scope.row.date }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="姓名"
-      width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, tableData)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
- </div>
+  <div class="table">
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column label="日期" width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="姓名" width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>姓名: {{ scope.row.name }}</p>
+            <p>住址: {{ scope.row.address }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.name }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="addRow(scope.$index, scope.row)">
+            添加
+          </el-button>
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+            >编辑</el-button
+          >
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
-
 <script>
-  export default {
-    data() {
-      return {
-        tableData: [{
+export default {
+  data() {
+    return {
+      tableData: [
+        {
           date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
           date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
+          name: '大力士',
+          address: '上海市普陀区金沙江路 1517 弄',
+        },
+        {
           date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
+          name: '傻乎乎',
+          address: '上海市普陀区金沙江路 1519 弄',
+        },
+        {
           date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
-      }
+          name: '真可爱',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
+    };
+  },
+  methods: {
+    // 获取表格选中时的数据
+
+    addRow(index, row) {
+      this.tableData.unshift(this.tableData[index]);
+      console.log(index, row);
     },
-    methods: {
-      handleEdit(index, row) {
-        console.log(index, row);
-      },
-      handleDelete(index, row) {
-        console.log(index, row);
-           row.splice(index, 1);
-      }
-    }
-  }
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+      this.tableData.splice(index, 1);
+    },
+  },
+};
 </script>
 <style lang="less" scoped>
-  .table{
-  
-   
-  }
+.table {
+}
 </style>
